55e2194bf6d5561720c208c9a64d3183c4fb3ac8
alekscarlston@yahoo.com <alekscarlston@yahoo.com>
2018-09-24 16:36:42 -0600

    got number buttons working and clear button

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,36 +1,58 @@
 <template>
   <div id="calculator">
     <div id="screen-container">
-      <div id="screen"></div>
+      <div id="screen">{{screenValue}}</div>
     </div>
 
     <div id="buttons-container">
       <div class="buttons">
-        <span class="operator" id="clear">C</span>
+        <span class="operator" id="clear" @click="clear">C</span>
         <span class="operator">/</span>
         <span class="operator">*</span>
-        <span>7</span>
-        <span>8</span>
-        <span>9</span>
+        <span @click="addNumber(7)">7</span>
+        <span @click="addNumber(8)">8</span>
+        <span @click="addNumber(9)">9</span>
         <span class="operator">-</span>
-        <span>4</span>
-        <span>5</span>
-        <span>6</span>
+        <span @click="addNumber(4)">4</span>
+        <span @click="addNumber(5)">5</span>
+        <span @click="addNumber(6)">6</span>
         <span class="operator">+</span>
-        <span>1</span>
-        <span>2</span>
-        <span>3</span>
+        <span @click="addNumber(1)">1</span>
+        <span @click="addNumber(2)">2</span>
+        <span @click="addNumber(3)">3</span>
         <span class="operator" id="equals">=</span>
         <div class="l-row">
-          <span id="zero">0</span>
+          <span id="zero" @click="addNumber(0)">0</span>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
+
 export default {
-  name: "Calculator"
+  name: "Calculator",
+  data(){
+    return {
+      screenValue: 0,
+      calculateValue: 0,
+      totalValue: 0
+    }
+  },
+  methods: {
+    addNumber(number){
+      if(this.screenValue === 0){
+        this.screenValue = number
+      } else {
+        var screen = document.querySelector("#screen")
+        screen.innerText += number
+      }
+
+    },
+    clear(){
+      this.screenValue = 0
+    }
+  }
 }
 </script>
 <style>
