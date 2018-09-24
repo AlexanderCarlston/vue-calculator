98e540f459c86a26d48fd64b7fe5e0ec6bd1d27d
alekscarlston@yahoo.com <alekscarlston@yahoo.com>
2018-09-24 14:22:22 -0600

    fixed indentation

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,36 +1,36 @@
 <template>
-    <div id="calculator">
-      <div id="screen-container">
-        <div id="screen"></div>
-      </div>
+  <div id="calculator">
+    <div id="screen-container">
+      <div id="screen"></div>
+    </div>
 
-      <div id="buttons-container">
-        <div class="buttons">
-          <span class="operator" id="clear">C</span>
-          <span class="operator">/</span>
-          <span class="operator">*</span>
-          <span>7</span>
-          <span>8</span>
-          <span>9</span>
-          <span class="operator">-</span>
-          <span>4</span>
-          <span>5</span>
-          <span>6</span>
-          <span class="operator">+</span>
-          <span>1</span>
-          <span>2</span>
-          <span>3</span>
-          <span class="operator" id="equals">=</span>
-          <div class="l-row">
-            <span id="zero">0</span>
-          </div>
+    <div id="buttons-container">
+      <div class="buttons">
+        <span class="operator" id="clear">C</span>
+        <span class="operator">/</span>
+        <span class="operator">*</span>
+        <span>7</span>
+        <span>8</span>
+        <span>9</span>
+        <span class="operator">-</span>
+        <span>4</span>
+        <span>5</span>
+        <span>6</span>
+        <span class="operator">+</span>
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+        <span class="operator" id="equals">=</span>
+        <div class="l-row">
+          <span id="zero">0</span>
         </div>
       </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    name: "Calculator"
+  name: "Calculator"
 }
 </script>
 <style>
